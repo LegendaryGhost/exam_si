7961d85a68578719f78624dc01502637e0d2cd77
Angelo RABEARISON <151778901+rAnjeel@users.noreply.github.com>
2024-04-24 11:48:09 +0300

    changement lien

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,10 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Home - Brand</title>
-    <link rel="stylesheet" href="../../../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700&amp;display=swap">
-    <link rel="stylesheet" href="../../../assets/css/Ludens-basic-login.css">
-    <link rel="stylesheet" href="../../../assets/css/pikaday.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/Ludens-basic-login.css">
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/pikaday.min.css">
 </head>
 
 <body>
@@ -24,9 +24,9 @@
             </div>
         </div>
     </nav>
-    <script src="../../../assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="../../../assets/js/pikaday.min.js"></script>
-    <script src="../../../assets/js/theme.js"></script>
+    <script src="<?= base_url() ?>assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?= base_url() ?>assets/js/pikaday.min.js"></script>
+    <script src="<?= base_url() ?>assets/js/theme.js"></script>
 </body>
 
 </html>
